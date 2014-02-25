d7934111bf3bd26d39ba8f26e48003d7ce68ee90
tianling <2507073658@qq.com>
2014-02-26 04:44:07 +0800

    StackWork

---
+++ StackWork/StackWork/StackWork/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  StackWork
+//
+//  Created by tianling on 14-2-26.
+//  Copyright (c) 2014年 tianling. All rights reserved.
+//  实现栈的生成，插入，删除，输出操作
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
