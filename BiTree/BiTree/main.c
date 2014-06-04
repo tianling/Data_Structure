47af69b76ea07cf6c7d532ae9964935095cb7954
tianling <2507073658@qq.com>
2014-06-04 11:47:42 +0800

    Bitree traverse

--- BiTree/BiTree/main.c
+++ BiTree/BiTree/main.c
@@ -14,9 +14,10 @@
 #define OK 0
 
 typedef int status;
-int NodeNum= 0,buildTime = 0;
+int NodeNum= 0;
 char dataArray[MaxSize];
 char *data = dataArray;
+
 
 /*
  **声明二叉树结点结构
@@ -26,12 +27,15 @@ typedef struct BiNode{
     struct BiNode *lChild,*rChild;//左右孩子结点指针
 }BitNode,*Bitree;
 
+
 /*
  **函数声明
  */
 void dataArrayGet();
 void BiTreeCreate(Bitree *T);
 void preOrderTraverse(Bitree T);
+void inOrderTraverse(Bitree T);
+void postOrderTraverse(Bitree T);
 status List();
 
 status main(int argc, const char * argv[])
@@ -41,19 +45,43 @@ status main(int argc, const char * argv[])
     int choose;
     
     dataArrayGet();
+    
+    //判断若头节点为空，则二叉树为空树
+    if(*data == '#'){
+        printf("空树！");
+        exit(ERROR);
+    }
+    
     BiTreeCreate(&Tree);
     choose = List();
     
-    switch (choose) {
-        case 1:
-            preOrderTraverse(Tree);
-            break;
-            
-        default:
-            break;
+    while(choose != 0){
+        
+        switch (choose) {
+            case 1:
+                preOrderTraverse(Tree);
+                break;
+                
+            case 2:
+                inOrderTraverse(Tree);
+                break;
+                
+            case 3:
+                postOrderTraverse(Tree);
+                break;
+                
+            case 0:
+                exit(OK);
+                
+            default:
+                break;
+        }
+        
+        choose = List();
+
     }
     
-    
+    return OK;
 }
 
 
@@ -65,10 +93,16 @@ status List(){
     
     printf("********菜单函数*******\n");
     printf("********1.前序遍历*****\n");
+    printf("********2.中序遍历*****\n");
+    printf("********3.后序遍历*****\n");
+    printf("********0.退出*********\n");
     
     scanf("%d",&choose);
     return choose;
 }
+
+
+
 /*
  **获取待建树数据
  */
@@ -83,12 +117,12 @@ void dataArrayGet(){
     
     while(fscanf(file, "%c",&temp) != EOF){
         *data = temp;
-        data+=1;
+        data++;
         NodeNum++;
     }
     
     fclose(file);
-    data -= NodeNum;
+    data -= NodeNum;//指针移位至数组头地址
     
 }
 
@@ -98,12 +132,11 @@ void dataArrayGet(){
  */
 void BiTreeCreate(Bitree *T){
 
-    if(buildTime<NodeNum){
+    if(*data != '\0'){
         //若头结点值为空值，则二叉树为空树
         if(*data == '#'){
             *T = NULL;
             data++;
-            buildTime++;
             
         }else{
             *T = (Bitree)malloc(sizeof(BitNode));
@@ -113,8 +146,7 @@ void BiTreeCreate(Bitree *T){
             
             (*T)->data = *data;
             data++;
-            buildTime++;
-        
+            
             //递归建立左子树和右子树(**其实就是按照前序遍历的规则啦**)
             BiTreeCreate(&(*T)->lChild);
             BiTreeCreate(&(*T)->rChild);
@@ -129,11 +161,44 @@ void BiTreeCreate(Bitree *T){
  */
 void preOrderTraverse(Bitree T){
     if(T == NULL){
-        //printf("空树");
+        printf(" ");
         return;
     }
     printf("%c",T->data);
     
     preOrderTraverse(T->lChild);
     preOrderTraverse(T->rChild);
+}
+
+
+/*
+ **中序遍历二叉树
+ */
+void inOrderTraverse(Bitree T){
+    if(T == NULL){
+        printf(" ");
+        return;
+    }
+    
+    //分别中序遍历左子树和右子树
+    inOrderTraverse(T->lChild);
+    printf("%c",T->data);
+    inOrderTraverse(T->rChild);
+}
+
+
+/*
+ **后序遍历二叉树
+ */
+void postOrderTraverse(Bitree T){
+    if(T == NULL){
+        printf(" ");
+        return;
+    }
+    
+    //分别先后续遍历左子树再后续遍历右子树
+    postOrderTraverse(T->lChild);
+    postOrderTraverse(T->rChild);
+    printf("%c",T->data);
+    
 }
